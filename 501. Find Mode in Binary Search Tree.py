990000bf9fad82288107b83aaef244b5c18ff3a1
Robertzi <549899794@qq.com>
2018-08-04 17:36:41 +0800

    8-4

---
+++ 501. Find Mode in Binary Search Tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findMode(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        self.list = []
+        self.prev = None
+        self.times = []
+        if root != None:
+            self.node(root)
+        if self.times[0] < self.times[-1]:
+            self.list.pop()
+        elif self.times[0] > self.times[-1]:
+            self.list = self.list[0:-1]
+        return self.list
+
+    def node(self, node):
+        if node.left != None:
+            self.node(node.left)
+        if node.val != None:
+            if(self.list != []):
+                if self.list[-1] == node.val:
+                    self.times[-1] += 1
+                else:
+                    if self.times[0] < self.times[-1]:
+                        self.list.pop()
+                        self.times.pop()
+                    elif self.times[0] > self.times[-1]:
+                        self.list = self.list[0:-1]
+                        self.times = self.times[0:-1]
+                    self.list.append(node.val)
+                    self.times.append(1)
+            else:
+                self.times.append(1)
+                self.list.append(node.val)
+        if node.right != None:
+            self.node(node.right)
--- 636.Exclusive Time of Functions.py
+++ 636.Exclusive Time of Functions.py
@@ -5,18 +5,24 @@ class Solution:
         :type logs: List[str]
         :rtype: List[int]
         """
-        arr = []
-        ll = len(logs)
-        res = [0 for i in range(n)]
-        run = None
-        for i in range(ll):
-            arr.append() = logs[i].split(':')
-            if(arr[i][1] == 'start'):
-                run = arr[i][0]
-            elif:
+        stack = []
+        res = [0] * n
+        lasttime = 0
+        for log in logs:
+            target = log.split(':')
+            target[0] = int(target[0])
+            target[2] = int(target[2])
+            if target[1] == 'start':
+                if len(stack) > 0:
+                    res[stack[-1]] += target[2] - lasttime - 1
+                stack.append(target[0])
+            else:
+                top = stack.pop()
+                res[top] += target[2] - lasttime + 1
+            lasttime = target[2]
+        return res
 
-            
 
-logs = ["0:start:0","1:start:2","1:end:5","0:end:6"]
+logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
 x = Solution()
-print(x.exclusiveTime(2,logs))
+print(x.exclusiveTime(2, logs))
